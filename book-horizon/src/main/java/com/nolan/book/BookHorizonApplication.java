6384b1daf75a33cec37ba6b6a26091cd54c6b9d6
nghilt7 <nghi.tanlam@gmail.com>
2024-09-12 10:29:54 +0700

    Initial Project

---
+++ book-horizon/src/main/java/com/nolan/book/BookHorizonApplication.java
@@ -0,0 +1,13 @@
+package com.nolan.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHorizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHorizonApplication.class, args);
+	}
+
+}
---
+++ book-horizon/src/test/java/com/nolan/book/BookHorizonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolan.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHorizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
