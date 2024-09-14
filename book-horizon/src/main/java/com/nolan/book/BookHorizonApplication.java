51166b0fb2b8dee0fe9076618baf2991956be3f2
nghilt7 <nghi.tanlam@gmail.com>
2024-09-14 20:43:39 +0700

    Complete BE-002

--- book-horizon/src/main/java/com/nolan/book/BookHorizonApplication.java
+++ book-horizon/src/main/java/com/nolan/book/BookHorizonApplication.java
@@ -1,13 +1,31 @@
 package com.nolan.book;
 
+import com.nolan.book.role.Role;
+import com.nolan.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class BookHorizonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookHorizonApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	}
 }
---
+++ book-horizon/src/main/java/com/nolan/book/token/Token.java
@@ -0,0 +1,32 @@
+package com.nolan.book.token;
+
+import com.nolan.book.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
